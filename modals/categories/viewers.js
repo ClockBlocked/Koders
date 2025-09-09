773682000abf06e976f0122e068df397ade7549f
Cole Hanson <4theloveofcarl@gmail.com>
2025-09-09 00:02:40 -0500

    Create viewers.js for viewer modals
    Added viewer modals for displaying playlists and artists.

---
+++ modals/categories/viewers.js
@@ -0,0 +1,23 @@
+//  Viewer Modals
+// 
+//  i.e.
+//  Viewing all playlists, Viewing favorite artists, ETC.
+//  Viewing any content in a mini-diaplay popup, ETC.
+// 
+//
+//
+//
+//
+import { appState } from '../../global.js'; // Example
+import { core } from '../global js'; // Example
+
+
+
+
+//
+// LOGIC FOR VIEWER MODALS GOES HERE
+//
+
+
+
+export { forms }; // Example
