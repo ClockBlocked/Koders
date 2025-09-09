021e17231e06329071565fd71a9ab07a2860b19b
Cole Hanson <4theloveofcarl@gmail.com>
2025-09-08 23:57:05 -0500

    Create forms.js

---
+++ modals/categories/forms.js
@@ -0,0 +1,23 @@
+//  Form Modals
+// 
+//  i.e.
+//  Creating a playlist, Renaming a playlist, 
+//  Editing playlist metadata / description, ETC.
+// 
+//
+//
+//
+//
+import { appState } from '../../global.js'; // Example
+import { core } from '../global js'; // Example
+
+
+
+
+//
+// LOGIC FOR FORM MODALS GOES HERE
+//
+
+
+
+export { forms }; // Example
