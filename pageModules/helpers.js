6ff952114d2ab2df18f3d39bd0a0073a7a3a793f
Cole Hanson <4theloveofcarl@gmail.com>
2025-09-18 02:31:49 -0500

    Delete pageModules directory

--- pageModules/helpers.js
+++
@@ -1,6 +0,0 @@
-// helpers.js
-export function create(htmlString) {
-  const template = document.createElement("template");
-  template.innerHTML = htmlString.trim();
-  return template.content.firstElementChild;
-}
